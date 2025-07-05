ed62d119fd6ed220ae5a7660638671ac26341f4c
KrishnaKant <krishnaplayz333@gmail.com>
2025-07-05 15:00:52 +0530

    Jarvis 0.2 Update

--- Mega Project 1 - Jarvis/main.py
+++ Mega Project 1 - Jarvis/main.py
@@ -8,6 +8,7 @@ import speech_recognition as sr
 import webbrowser
 import pyttsx3
 import musicLibrary
+import subprocess
 
 # recognizer = sr.Recognizer()
 engine = pyttsx3.init()
@@ -22,30 +23,50 @@ def openSite(c):
 
 def processCommand(w):
 	w = w.lower()
-	if w.startswith("open"):
-		site = w.split(" ")[1]
-		if site in ["google" , "youtube" , "facebook" , "linkedin"]:
-			txt = f"Opening {site}.."
+
+	# if w
+
+
+
+	if w.startswith("open") or w.startswith("launch") or w.startswith("fire"):
+		name = w.split(" ")[1]
+		if name in ["google" , "youtube" , "facebook" , "linkedin", "github"]:
+			txt = f"Opening {name}.."
 			printAndSpeak(txt)
-			openSite(site)
+			openSite(name)
 			return
-		if site == "minecraft":
-			txt = "Opening Minecraft.."
+
+		if name == "notepad":
+			subprocess.Popen(["notepad.exe"])
+			printAndSpeak("Launching Notepad...")
+			return
+		elif name == "calculator":
+			subprocess.Popen(["calculator.exe"])
+			printAndSpeak("Launching Calculator...")
+			return
+		elif name == "code":
+			subprocess.Popen(["C:\\Users\\Krishna Kant\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"])
+			printAndSpeak("Launching Visual Studio Code...")
+			return
+
+		if name == "server":
+			txt = "Launching Minecraft Server.."
 			printAndSpeak(txt)
 			webbrowser.open("https://aternos.org")
 			return
 
-		printAndSpeak("Sorry, I don't have the permission to open that site.")
+		printAndSpeak("Sorry, I don't have the permission to open that.")
 
 	elif w.startswith("play"):
 		song = w.split(" ")[1]
 		if song in musicLibrary.music:
 			link = musicLibrary.music[song]
-			txt = f"Playing {link}.."
+			txt = f"Playing {song.title()}.."
 			printAndSpeak(txt)
 			webbrowser.open(link)
 			return
 		printAndSpeak("Sorry, that song is not in my Library.")
+
 
 	elif w in ["bye", "quit", "end", "exit"]:
 		txt = "Goodbye, shutting down.."
@@ -57,24 +78,24 @@ def processCommand(w):
 
 if __name__ == "__main__":
 	printAndSpeak("Initializing Jarvis...")
+	r = sr.Recognizer()
 	while True:
 		# Listen for the wake word "Jarvis"
 		# Obtain audio from microphone :
-		r = sr.Recognizer()
 
 		print("Recognizing...")
 		# Recognize speech :
 		try:
 			with sr.Microphone() as source:
 				print("Listening..")
-				audio = r.listen(source, timeout=5, phrase_time_limit=4)
+				audio = r.listen(source, timeout=3, phrase_time_limit=3)
 			word = r.recognize_google(audio)
 			if (isinstance(word, str) and word.lower() == "jarvis"):
 				printAndSpeak("Yes!")
 				# Listen for command :
 				with sr.Microphone() as source:
 					print("Jarvis is listening..")
-					audio = r.listen(source, timeout=5, phrase_time_limit=4)
+					audio = r.listen(source, timeout=3, phrase_time_limit=3)
 					command = r.recognize_google(audio)
 
 					processCommand(command)
